b48f040a501702cc16a0de1432bb449ae84d45fd
yieldtycoon <mudassir@yieldarcade.com>
2024-10-08 19:45:23 +0500

    add withdraw method

--- src/interfaces/external/EtherFi/ILiquidityPool.sol
+++ src/interfaces/external/EtherFi/ILiquidityPool.sol
@@ -7,6 +7,8 @@ interface ILiquidityPool {
     function deposit(address _user, address _referral) external payable returns (uint256);
     function depositToRecipient(address _recipient, uint256 _amount, address _referral) external returns (uint256);
 
+    function withdraw(address _recipient, uint256 _amount) external returns (uint256);
+
     function getTotalPooledEther() external view returns (uint256);
     function sharesForAmount(uint256 _amount) external view returns (uint256);
 }
