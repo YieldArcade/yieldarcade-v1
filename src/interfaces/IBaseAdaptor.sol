8e8b5a80979e48318702ced61fbfbf3d55021717
yieldtycoon <mudassir@yieldarcade.com>
2024-10-08 19:44:38 +0500

    add exchangeRate method

--- src/interfaces/IBaseAdaptor.sol
+++ src/interfaces/IBaseAdaptor.sol
@@ -4,9 +4,11 @@ pragma solidity =0.8.25;
 import { ERC20 } from "@solmate/tokens/ERC20.sol";
 
 interface IBaseAdaptor {
-    function deposit(uint256 amount, bytes memory adaptorData) external;
+    function deposit(address depositAsset, uint256 amount, bytes memory adaptorData) external payable;
 
     function withdraw(uint256 amount, address receiver, bytes memory adaptorData) external;
 
-    function assetOf(bytes memory adaptorData) external view returns (ERC20);
+    function assetInfo(bytes memory adaptorData) external view returns (ERC20);
+
+    function exchangeRate(uint256 amount) external view returns (uint256);
 }
